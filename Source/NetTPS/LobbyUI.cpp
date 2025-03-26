acf20c4c07461262d2908e49c34d0ccf25377fb9
lokimve7 <lokimve7@gmail.com>
2025-03-26 09:46:11 +0900

    .

--- Source/NetTPS/LobbyUI.cpp
+++ Source/NetTPS/LobbyUI.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "LobbyUI.h"
@@ -8,6 +8,7 @@
 #include "Components/EditableTextBox.h"
 #include "Components/Slider.h"
 #include "Components/TextBlock.h"
+#include "Components/WidgetSwitcher.h"
 
 void ULobbyUI::NativeConstruct()
 {
@@ -19,6 +20,22 @@ void ULobbyUI::NativeConstruct()
 	Btn_Create->OnClicked.AddDynamic(this, &ULobbyUI::CreateSession);
 	// Slider 의 값이 변경될 때마다 호출되는 함수 등록
 	Slider_PlayerCount->OnValueChanged.AddDynamic(this, &ULobbyUI::OnValueChanged);
+
+	Btn_GoCreate->OnClicked.AddDynamic(this, &ULobbyUI::OnClickGoCreateRoom);
+	Btn_GoFind->OnClicked.AddDynamic(this, &ULobbyUI::OnClickGoFindRoom);
+	Btn_Find->OnClicked.AddDynamic(this, &ULobbyUI::OnClickFind);
+	Btn_BackFromCreate->OnClicked.AddDynamic(this, &ULobbyUI::OnClickBack);
+	Btn_BackFromFind->OnClicked.AddDynamic(this, &ULobbyUI::OnClickBack);
+}
+
+void ULobbyUI::OnClickGoCreateRoom()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(1);
+}
+
+void ULobbyUI::OnClickGoFindRoom()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(2);
 }
 
 void ULobbyUI::CreateSession()
@@ -39,6 +56,16 @@ void ULobbyUI::OnValueChanged(float Value)
 {
 	// Value 값을 text 값으로 변경 후 셋팅
 	Text_PlayerCount->SetText(FText::AsNumber(Value));
+}
+
+void ULobbyUI::OnClickFind()
+{
+	gi->FindOtherSession();
+}
+
+void ULobbyUI::OnClickBack()
+{
+	WidgetSwitcher->SetActiveWidgetIndex(0);
 }
 
 
--- Source/NetTPS/LobbyUI.h
+++ Source/NetTPS/LobbyUI.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -21,6 +21,24 @@ public:
 	class UNetGameInstance* gi;
 	
 public:
+	
+	// 위젯 스위쳐
+	UPROPERTY(meta = (BindWidget))
+	class UWidgetSwitcher* WidgetSwitcher;
+		
+
+	// 세션 메인 관련
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_GoCreate;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_GoFind;
+	UFUNCTION()
+	void OnClickGoCreateRoom();
+	UFUNCTION()
+	void OnClickGoFindRoom();
+
+
+	// 세션 생성 관련
 	UPROPERTY(meta = (BindWidget))
 	class UEditableTextBox* Edit_RoomName;
 
@@ -32,12 +50,25 @@ public:
 
 	UPROPERTY(meta = (BindWidget))
 	class UButton* Btn_Create;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_BackFromCreate;
 
 	UFUNCTION()
 	void CreateSession();
 
 	UFUNCTION()
 	void OnValueChanged(float Value);
+
+	// 세션 검색 관련
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_Find;
+	UPROPERTY(meta = (BindWidget))
+	class UButton* Btn_BackFromFind;
+
+	UFUNCTION()
+	void OnClickFind();
+	UFUNCTION()
+	void OnClickBack();
 };
 
 
